9bf99d05096c3ffa81cbc16e75fe1bc49a674076
ernyson <ernestaiji1@gmail.com>
2022-06-02 14:28:56 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - print line strings the puts function
-  * Description: prints "programming is like building a multilingual puzzle
+  * Description: prints "Programming is like building a multilingual puzzle
   * Return: 0
   */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
