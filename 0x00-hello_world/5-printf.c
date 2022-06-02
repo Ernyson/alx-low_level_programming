8ee7f0612de35539746c7f32f61df441151736a6
ernyson <ernestaiji1@gmail.com>
2022-06-02 14:52:19 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the puts funtion
+ *
+ * Description: using main function this program prints
+ * "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingualpuzzle");
+return (0);
+}
