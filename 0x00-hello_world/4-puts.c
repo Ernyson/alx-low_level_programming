656a0c872ad0ab7ad5b1826999685b6f20ce061d
ernyson <ernestaiji1@gmail.com>
2022-06-02 13:38:55 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print line strings the puts function 
+  * Description: prints "programming is like building a multilingual puzzle
+  * Return: 0
+  */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
