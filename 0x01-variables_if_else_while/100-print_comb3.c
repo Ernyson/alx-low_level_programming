7acb0e08cbb372578c2aade839714db3c61536a7
ernyson <ernestaiji1@gmail.com>
2022-06-03 20:12:00 -0700

    great

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two different digits,
+ * main - Prints all possible combinations of two different digits,
  * in ascending order, seperated by a comma followed by a space.
  *
  * Return: Always 0 (Succes)
