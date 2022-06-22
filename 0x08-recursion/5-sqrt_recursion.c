cd509d4c79f55442ca04dc72ef48dab3978061be
ernyson <ernestaiji1@gmail.com>
2022-06-22 15:37:37 -0700

    high

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,18 @@
 #include "main.h"
+#include <math.h>
 /**
- * _sqrt_recursion - return the natural square root of a number
- * @n: int number
- * Return: if no natural square root, return -1 else square root
+ * _sqrt_recursion - return the natural sqaure root of a number
+ * @n: argument to the function
+ *
+ * Description: return the required result
+ *
+ * Return: return integer value
  */
 int _sqrt_recursion(int n)
 {
-return (halp(n, 1));
-}
-/**
- * halp - function to solve sqrt_recursion
- * @c: number to determine if squareroot
- * @i: Incrementer to compare against c
- * Return: square root if natural or -1 i none found
- */
-int halp(int c, int i)
-{
-int square;
+	int x;
 
-square = i * i;
-if (square == c)
-return (i);
-else if (square < c)
-return (halp(c, i + 1));
-else
-return (-1);
+	x = sqrt(n);
+	if (n != x * x)
+		return (-1);
 }
