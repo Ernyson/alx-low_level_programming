8c050af39158eee5999120e3dbc3b7c911973263
ernyson <ernestaiji1@gmail.com>
2022-07-14 17:05:09 -0700

    list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * free_list - frees a list_t list
+ * @head: head of the linked list
+ */
+void free_list(list_t *head)
+{
+list_t *current;
+list_t *nxt;
+
+current = head;
+
+while (current != NULL)
+{
+nxt = current->next;
+free(current->str);
+free(current);
+current = nxt;
+}
+}
