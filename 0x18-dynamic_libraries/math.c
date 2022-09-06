7ff8be657542e5ec6dd355fba23454755b93419a
ernyson <ernestaiji1@gmail.com>
2022-09-05 18:02:09 -0700

    new file added

---
+++ 0x18-dynamic_libraries/math.c
@@ -0,0 +1,39 @@
+int add(int a, int b)
+{
+int sum;
+
+sum = a + b;
+return (sum);
+}
+
+int sub(int a, int b)
+{
+int sum;
+
+sum = a - b;
+return (sum);
+}
+
+int mul(int a, int b)
+{
+int sum;
+
+sum = a * b;
+return (sum);
+}
+
+int div(int a, int b)
+{
+int sum;
+
+sum = a / b;
+return (sum);
+}
+
+int mod(int a, int b)
+{
+int sum;
+
+sum = a % b;
+return (sum);
+}
